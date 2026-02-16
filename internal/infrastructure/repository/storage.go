dc1fad7ed8535846164280ba6d61848c3ae92613
kropt <SlavaCh2018@yandex.ru>
2026-02-16 20:25:53 +0500

    refactor(storage): change return type to error and add context

--- internal/infrastructure/repository/storage.go
+++ internal/infrastructure/repository/storage.go
@@ -1,32 +1,43 @@
 package repository
 
 import (
+	"context"
 	"dudanseai_bot/internal/domain"
 	"dudanseai_bot/pkg/syncmap"
+	"errors"
 )
 
 type Storage struct {
 	Users syncmap.SyncMap[domain.UserID, domain.User]
 }
 
-func NewStorage() *Storage {
-	return &Storage{Users: syncmap.NewSyncMap[domain.UserID, domain.User]()}
+func NewStorage() Storage {
+	return Storage{Users: syncmap.NewSyncMap[domain.UserID, domain.User]()}
 }
 
-func (s *Storage) UpdateUser(u domain.User) {
+func (s *Storage) UpdateUser(ctx context.Context, u domain.User) error {
 	s.Users.Set(u.ID, u)
+	return nil
 }
 
-func (s *Storage) CreateUser(u domain.User) {
+func (s *Storage) CreateUser(ctx context.Context, u domain.User) error {
 	s.Users.Set(u.ID, u)
+	return nil
 }
 
-func (s *Storage) DeleteUser(id domain.UserID) {
+func (s *Storage) DeleteUser(ctx context.Context, id domain.UserID) error {
 	if s.Users.Exists(id) {
 		s.Users.Delete(id)
+	} else {
+		return errors.New("user not found")
 	}
+	return nil
 }
 
-func (s *Storage) GetUser(id domain.UserID) (domain.User, bool) {
-	return s.Users.Get(id)
+func (s *Storage) GetUser(ctx context.Context, id domain.UserID) (domain.User, error) {
+	user, ok := s.Users.Get(id)
+	if !ok {
+		return domain.User{}, errors.New("user not found")
+	}
+	return user, nil
 }
