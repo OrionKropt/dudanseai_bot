e39b73821ac4c47a3b21a6658376bb57fa0c3a23
kropt <SlavaCh2018@yandex.ru>
2026-02-16 20:33:51 +0500

    feat(usecase) add offer lead magnet

---
+++ internal/usecase/offer_lead_magnet.go
@@ -0,0 +1,34 @@
+package usecase
+
+import (
+	"context"
+	"dudanseai_bot/internal/domain"
+	"dudanseai_bot/pkg/logger"
+	"errors"
+	"fmt"
+	"net/url"
+)
+
+func (c *Core) OfferLeadMagnet(ctx context.Context, user domain.User) (msg *domain.Message, err error) {
+	guideTitle := "Lead magnet 1"
+	if user, err = c.fetchUserByID(ctx, user.ID); err != nil {
+		return nil, err
+	}
+
+	c.log.Log(logger.INFO, "offer lead magnet", "id", user.ID.String(), "username", user.Username)
+	guide, err := c.materialRepo.FindMaterialByTitle(ctx, guideTitle)
+	if err != nil {
+		msgErr := fmt.Sprintf("Failed to get %s", guideTitle)
+		c.log.Log(logger.ERROR, msgErr, "error", err.Error())
+		return nil, errors.New(msgErr)
+	}
+	keyboard := domain.NewKeyboard()
+	link, err := url.Parse(guide.URL.String())
+	if err != nil {
+		msgErr := "failed to parse URL"
+		c.log.Log(logger.ERROR, msgErr, "error", err.Error())
+		return nil, errors.New(msgErr)
+	}
+	keyboard.Row().AddButtonURL("📥 Забрать гайд ", domain.ActionGetGuide, link)
+	return domain.NewMessage(user.ChatID, guide.Description, keyboard), nil
+}
