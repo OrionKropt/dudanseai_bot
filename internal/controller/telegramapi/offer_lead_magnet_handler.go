9ea736381956deb396131c2ee3a24f851d49ac1f
kropt <SlavaCh2018@yandex.ru>
2026-02-24 07:36:26 +0500

    feat(tgapi): add delayed course offer after video note

--- internal/controller/telegramapi/offer_lead_magnet_handler.go
+++ internal/controller/telegramapi/offer_lead_magnet_handler.go
@@ -3,8 +3,10 @@ package telegramapi
 import (
 	"context"
 	"dudanseai_bot/internal/domain"
+	"time"
 
 	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
 )
 
 func (b *BotAPI) OfferLeadMagnetHandler(ctx context.Context, user domain.User) {
@@ -13,8 +15,30 @@ func (b *BotAPI) OfferLeadMagnetHandler(ctx context.Context, user domain.User) {
 		return
 	}
 
-	err = b.sendMessage(&bot.SendMessageParams{ChatID: msg.ChatID, Text: msg.Text, ReplyMarkup: b.CreateInlineMarkab(msg.Keys)})
+	err = b.sendMessage(&bot.SendMessageParams{ChatID: user.ChatID, Text: msg.Text, ReplyMarkup: b.CreateInlineMarkab(msg.Keys)})
 	if err != nil {
 		b.LoggingErrors(err)
 	}
+
+	detachedCtx := context.WithoutCancel(ctx)
+	time.AfterFunc(time.Hour*24, func() {
+		note, err := b.uc.PresentSystem(detachedCtx, user)
+		if err == nil {
+			err = b.sendVideoNote(&bot.SendVideoNoteParams{ChatID: user.ChatID, VideoNote: &models.InputFileString{Data: note.File.TelegramFileID}})
+			if err != nil {
+				b.LoggingErrors(err)
+			}
+		}
+		
+		time.AfterFunc(time.Hour*24, func() {
+			msg, err = b.uc.OfferCourse(detachedCtx, user)
+			if err != nil {
+				return
+			}
+			err = b.sendMessage(&bot.SendMessageParams{ChatID: user.ChatID, Text: msg.Text, ReplyMarkup: b.CreateInlineMarkab(msg.Keys)})
+			if err != nil {
+				b.LoggingErrors(err)
+			}
+		})
+	})
 }
