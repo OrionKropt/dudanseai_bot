29e50f8185806edbfdfa160db6c691c4fff0c0de
kropt <SlavaCh2018@yandex.ru>
2026-03-18 10:40:15 +0500

    feat(tgapi): add logging file id of video message

--- internal/controller/telegramapi/middleware/logging.go
+++ internal/controller/telegramapi/middleware/logging.go
@@ -17,6 +17,10 @@ func LoggingMiddleware(log *logger.Logger) func(next bot.HandlerFunc) bot.Handle
 				log.Log(logger.INFO, "empty message")
 			} else {
 				log.Log(logger.INFO, fmt.Sprintf("message %s from %s", msg.Text, msg.From.Username))
+				if update.Message.VideoNote != nil {
+					fileID := update.Message.VideoNote.FileID
+					log.Log(logger.INFO, fmt.Sprintf("video file id: %s", fileID))
+				}
 			}
 
 			next(ctx, b, update)
